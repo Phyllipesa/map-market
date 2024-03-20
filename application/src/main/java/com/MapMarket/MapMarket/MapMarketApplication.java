be9c8d27054cb501d83903e5d67b4f0ab26cffbe
Phyllipe <phyllipe.sjc@hotmail.com>
2024-03-20 18:20:27 -0300

    First commit

---
+++ application/src/main/java/com/MapMarket/MapMarket/MapMarketApplication.java
@@ -0,0 +1,13 @@
+package com.MapMarket.MapMarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapMarketApplication.class, args);
+	}
+
+}
---
+++ application/src/test/java/com/MapMarket/MapMarket/MapMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MapMarket.MapMarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

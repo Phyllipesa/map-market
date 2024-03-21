9aab21ab34fc738ca6991a0802e131d756a384fd
Phyllipe <phyllipe.sjc@hotmail.com>
2024-03-21 18:56:19 -0300

    Criado o Dockerfile da aplicação

--- application/src/main/resources/db/migration/V14__Populate_Table_Itens_Aba.sql
+++ application/src/main/resources/db/migration/V14__Populate_Table_Itens_Aba.sql
@@ -3,122 +3,105 @@ INSERT INTO `Itens_Aba` (`id_aba`, `id_item`) VALUES
     (1, 1),
     (1, 2),
     (1, 3),
-    (1, 4);
+    (1, 4),
 
 -- Grupo 2
-INSERT INTO `Itens_Aba` (`id_aba`, `id_item`) VALUES
     (2, 5),
     (2, 6),
     (2, 7),
-    (2, 8);
+    (2, 8),
 
 -- Grupo 3
-INSERT INTO `Itens_Aba` (`id_aba`, `id_item`) VALUES
     (3, 9),
     (3, 10),
     (3, 11),
-    (3, 12);
+    (3, 12),
 
 -- Grupo 4
-INSERT INTO `Itens_Aba` (`id_aba`, `id_item`) VALUES
     (4, 13),
     (4, 14),
     (4, 15),
-    (4, 16);
+    (4, 16),
 
 -- Grupo 5
-INSERT INTO `Itens_Aba` (`id_aba`, `id_item`) VALUES
     (5, 17),
     (5, 18),
     (5, 19),
-    (5, 20);
+    (5, 20),
 
 -- Grupo 6
-INSERT INTO `Itens_Aba` (`id_aba`, `id_item`) VALUES
     (6, 21),
     (6, 22),
     (6, 23),
-    (6, 24);
+    (6, 24),
 
 -- Grupo 7
-INSERT INTO `Itens_Aba` (`id_aba`, `id_item`) VALUES
     (7, 25),
     (7, 26),
     (7, 27),
-    (7, 28);
+    (7, 28),
 
 -- Grupo 8
-INSERT INTO `Itens_Aba` (`id_aba`, `id_item`) VALUES
     (8, 29),
     (8, 30),
     (8, 31),
-    (8, 32);
+    (8, 32),
 
 -- Grupo 9
-INSERT INTO `Itens_Aba` (`id_aba`, `id_item`) VALUES
     (9, 33),
     (9, 34),
     (9, 35),
-    (9, 36);
+    (9, 36),
 
 -- Grupo 10
-INSERT INTO `Itens_Aba` (`id_aba`, `id_item`) VALUES
     (10, 37),
     (10, 38),
     (10, 39),
-    (10, 40);
+    (10, 40),
 
 -- Grupo 11
-INSERT INTO `Itens_Aba` (`id_aba`, `id_item`) VALUES
     (11, 41),
     (11, 42),
     (11, 43),
-    (11, 44);
+    (11, 44),
 
 -- Grupo 12
-INSERT INTO `Itens_Aba` (`id_aba`, `id_item`) VALUES
     (12, 45),
     (12, 46),
     (12, 47),
-    (12, 48);
+    (12, 48),
 
 -- Grupo 13
-INSERT INTO `Itens_Aba` (`id_aba`, `id_item`) VALUES
     (13, 49),
     (13, 50),
     (13, 51),
-    (13, 52);
+    (13, 52),
 
 -- Grupo 14
-INSERT INTO `Itens_Aba` (`id_aba`, `id_item`) VALUES
     (14, 53),
     (14, 54),
     (14, 55),
-    (14, 56);
+    (14, 56),
 
 -- Grupo 15
-INSERT INTO `Itens_Aba` (`id_aba`, `id_item`) VALUES
     (15, 57),
     (15, 58),
     (15, 59),
-    (15, 60);
+    (15, 60),
 
 -- Grupo 16
-INSERT INTO `Itens_Aba` (`id_aba`, `id_item`) VALUES
     (16, 61),
     (16, 62),
     (16, 63),
-    (16, 64);
+    (16, 64),
 
 -- Grupo 17
-INSERT INTO `Itens_Aba` (`id_aba`, `id_item`) VALUES
     (17, 65),
     (17, 66),
     (17, 67),
-    (17, 68);
+    (17, 68),
 
 -- Grupo 18
-INSERT INTO `Itens_Aba` (`id_aba`, `id_item`) VALUES
     (18, 69),
     (18, 70),
     (18, 71),

b54902113f2c61c3d2e873440b61f82f2a2ab270
Phyllipe <phyllipe.sjc@hotmail.com>
2024-04-01 21:42:19 -0300

    Criada ProdutoNotFoundException

---
+++ application/src/main/java/com/MapMarket/domain/exception/ProdutoNotFoundException.java
@@ -0,0 +1,21 @@
+package com.MapMarket.domain.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.io.Serial;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class ProdutoNotFoundException extends RuntimeException {
+
+  @Serial
+  private static final long serialVersionUID = 1L;
+
+  public ProdutoNotFoundException(String e) {
+    super(e);
+  }
+
+  public ProdutoNotFoundException(String e, Throwable cause) {
+    super(e, cause);
+  }
+}

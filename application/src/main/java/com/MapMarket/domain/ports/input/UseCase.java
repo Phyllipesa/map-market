e59114e7dc237009137a2f8d359e1af1b15a98b5
Phyllipe <phyllipe.sjc@hotmail.com>
2024-04-23 20:02:01 -0300

    Adição do metodo 'create' no UseCase

--- application/src/main/java/com/MapMarket/domain/ports/input/UseCase.java
+++ application/src/main/java/com/MapMarket/domain/ports/input/UseCase.java
@@ -1,12 +1,15 @@
 package com.MapMarket.domain.ports.input;
 
+import com.MapMarket.domain.exception.ParameterNotFoundException;
 import com.MapMarket.domain.exception.ProdutoNotFoundException;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
 public interface UseCase<T> {
 
-  List<T> findAll() throws ProdutoNotFoundException;
+//  List<T> findAll(Pageable pageable) throws ProdutoNotFoundException;
 
   T findById(Long id) throws ProdutoNotFoundException;
+  T create(T t) throws ParameterNotFoundException;
 }

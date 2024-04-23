41c2f59caf7924ec8285e68b91ac5ab6f8d543d9
Phyllipe <phyllipe.sjc@hotmail.com>
2024-04-23 20:04:56 -0300

    Criado CustomizedResponseEntityExceptionHandler

---
+++ application/src/main/java/com/MapMarket/domain/exception/handler/CustomizedResponseEntityExceptionHandler.java
@@ -0,0 +1,64 @@
+package com.MapMarket.domain.exception.handler;
+
+import com.MapMarket.domain.exception.ExceptionResponse;
+import com.MapMarket.domain.exception.ParameterNotFoundException;
+import com.MapMarket.domain.exception.ProdutoNotFoundException;
+import com.MapMarket.domain.exception.RequiredObjectIsNullException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.Date;
+
+@ControllerAdvice
+@RestController
+public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+
+  @ExceptionHandler(Exception.class)
+  public final ResponseEntity<ExceptionResponse> handleAllExceptions(Exception e, WebRequest request) {
+    ExceptionResponse exceptionResponse = new ExceptionResponse(
+        new Date(),
+        e.getMessage(),
+        request.getDescription(false)
+    );
+
+    return new ResponseEntity<>(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+  }
+
+  @ExceptionHandler(ProdutoNotFoundException.class)
+  public final ResponseEntity<ExceptionResponse> handleNotFoundExceptions(Exception e, WebRequest request) {
+    ExceptionResponse exceptionResponse = new ExceptionResponse(
+        new Date(),
+        e.getMessage(),
+        request.getDescription(false)
+    );
+
+    return new ResponseEntity<>(exceptionResponse, HttpStatus.NOT_FOUND);
+  }
+
+  @ExceptionHandler(ParameterNotFoundException.class)
+  public final ResponseEntity<ExceptionResponse> handleParameterNotFoundExceptions(Exception e, WebRequest request) {
+    ExceptionResponse exceptionResponse = new ExceptionResponse(
+        new Date(),
+        e.getMessage(),
+        request.getDescription(false)
+    );
+
+    return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST);
+  }
+
+  @ExceptionHandler(RequiredObjectIsNullException.class)
+  public final ResponseEntity<ExceptionResponse> handleBadRequestExceptions(Exception e, WebRequest request) {
+    ExceptionResponse exceptionResponse = new ExceptionResponse(
+        new Date(),
+        e.getMessage(),
+        request.getDescription(false)
+    );
+
+    return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST);
+  }
+}

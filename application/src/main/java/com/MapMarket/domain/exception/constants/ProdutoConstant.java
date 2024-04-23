ae1b3d38000c960adc41c83430ab3e08d5106c36
Phyllipe <phyllipe.sjc@hotmail.com>
2024-04-23 20:06:02 -0300

    Criado ProdutoConstant

---
+++ application/src/main/java/com/MapMarket/domain/exception/constants/ProdutoConstant.java
@@ -0,0 +1,14 @@
+package com.MapMarket.domain.exception.constants;
+
+public class ProdutoConstant {
+  public static final String PRODUCT_NOT_FOUND = "Product not found :: ";
+  public static final String REQUIRED_PARAMETER = "Required parameter '";
+  public static final String IS_NULL_OR_BLANK = "' is null or blank!";
+  public static final double D_2 = 0.0;
+  public static final int INT = 0;
+  public static final String THIS_PRODUCT_IS_ALREADY_REGISTERED = "This Product is already registered ";
+  public static final String PRODUCTS_NOT_FOUND = "Products not found";
+
+  private ProdutoConstant() {
+  }
+}

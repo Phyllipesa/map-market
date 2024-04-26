60069e9a580caec388ab55c4e77c04749fe7f445
Phyllipe <phyllipe.sjc@hotmail.com>
2024-04-25 21:21:50 -0300

    Adicionado metodo "create" e refatoração do metodo "findById" em ProdutoPersistenceAdapter

--- application/src/main/java/com/MapMarket/infrastructure/adapters/output/persistence/ProdutoPersistenceAdapter.java
+++ application/src/main/java/com/MapMarket/infrastructure/adapters/output/persistence/ProdutoPersistenceAdapter.java
@@ -2,10 +2,11 @@ package com.MapMarket.infrastructure.adapters.output.persistence;
 
 import com.MapMarket.domain.models.Produto;
 import com.MapMarket.domain.ports.output.OutputPort;
+import com.MapMarket.infrastructure.adapters.output.persistence.entities.ProdutoEntity;
 import com.MapMarket.infrastructure.adapters.output.persistence.mapper.EntityMapper;
 import com.MapMarket.infrastructure.adapters.output.persistence.repositories.ProdutoRepository;
 
-import java.util.List;
+import java.util.Optional;
 import java.util.logging.Logger;
 
 public class ProdutoPersistenceAdapter implements OutputPort<Produto> {
@@ -19,15 +20,22 @@ public class ProdutoPersistenceAdapter implements OutputPort<Produto> {
     this.entityMapper = entityMapper;
   }
 
+//  @Override
+//  public List<Produto> findAll() {
+//    logger.info("Finding all products! (Adapter)");
+//    return entityMapper.parseListObject(produtoRepository.findAll(), Produto.class);
+//  }
+
   @Override
-  public List<Produto> findAll() {
-    logger.info("Finding all products! (Adapter)");
-    return entityMapper.parseListObject(produtoRepository.findAll(), Produto.class);
+  public Optional<Produto> findById(Long id) {
+    logger.info("Finding a product by Id!");
+    return Optional.of(entityMapper.parseObject(produtoRepository.findById(id), Produto.class));
   }
 
   @Override
-  public Produto findById(Long id) {
-    logger.info("Finding a product by Id!");
-    return entityMapper.parseObject(produtoRepository.findById(id), Produto.class);
+  public Optional<Produto> create(Produto produto) {
+    logger.info("Creating a product!");
+    ProdutoEntity entity = entityMapper.parseObject(produto, ProdutoEntity.class);
+    return Optional.of(entityMapper.parseObject(produtoRepository.save(entity), Produto.class));
   }
 }

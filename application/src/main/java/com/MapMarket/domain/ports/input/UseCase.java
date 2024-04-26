966bec553606cbc9affd8167468bff3bc9306997
Phyllipe <phyllipe.sjc@hotmail.com>
2024-04-26 20:04:57 -0300

    Adicionado metodo "update" na interface UseCase

--- application/src/main/java/com/MapMarket/domain/ports/input/UseCase.java
+++ application/src/main/java/com/MapMarket/domain/ports/input/UseCase.java
@@ -6,7 +6,7 @@ import com.MapMarket.domain.exception.ResourceNotFoundException;
 public interface UseCase<T> {
 
 //  List<T> findAll(Pageable pageable) throws ResourceNotFoundException;
-
-  T findById(Long id) throws ResourceNotFoundException;
-  T create(T t) throws ParameterNotFoundException;
+  T findById(Long id);
+  T create(T t);
+  T update(Long id, T t);
 }

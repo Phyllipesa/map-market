91f0fb8e7dc4f661cde81083837c89ccf1423623
Phyllipe <phyllipe.sjc@hotmail.com>
2024-04-25 21:21:23 -0300

    Adicionado metodo "create" e refatoração do metodo "findById"

--- application/src/main/java/com/MapMarket/domain/service/ProdutoService.java
+++ application/src/main/java/com/MapMarket/domain/service/ProdutoService.java
@@ -1,35 +1,42 @@
 package com.MapMarket.domain.service;
 
-import com.MapMarket.domain.exception.ProdutoNotFoundException;
+import com.MapMarket.domain.exception.ProductCreationException;
+import com.MapMarket.domain.exception.ResourceNotFoundException;
+import com.MapMarket.domain.exception.constants.ProdutoConstant;
+import com.MapMarket.domain.logic.ValidationProduct;
 import com.MapMarket.domain.models.Produto;
 import com.MapMarket.domain.ports.input.UseCase;
 import com.MapMarket.domain.ports.output.OutputPort;
 
-import java.util.List;
-
 public class ProdutoService implements UseCase<Produto> {
 
   private final OutputPort<Produto> outputPort;
+  private final ValidationProduct validationProduct;
 
-  public ProdutoService(OutputPort<Produto> outputPort) {
+  public ProdutoService(OutputPort<Produto> outputPort, ValidationProduct validationProduct) {
     this.outputPort = outputPort;
+    this.validationProduct = validationProduct;
+  }
+
+//  @Override
+//  public List<Produto> findAll() throws ResourceNotFoundException {
+//    List<Produto> produtos = outputPort.findAll();
+//    if (produtos.isEmpty()) {
+//      throw new ResourceNotFoundException("Não foram encontrados produtos");
+//    }
+//    return produtos;
+//  }
+
+  @Override
+  public Produto findById(Long id) throws ResourceNotFoundException {
+    return outputPort.findById(id)
+        .orElseThrow(() -> new ResourceNotFoundException(ProdutoConstant.PRODUCT_NOT_FOUND + id));
   }
 
   @Override
-  public List<Produto> findAll() throws ProdutoNotFoundException {
-    List<Produto> produtos = outputPort.findAll();
-    if (produtos.isEmpty()) {
-      throw new ProdutoNotFoundException("Não foram encontrados produtos");
-    }
-    return produtos;
-  }
-
-  @Override
-  public Produto findById(Long id) throws ProdutoNotFoundException {
-    Produto produto = outputPort.findById(id);
-    if (produto == null) {
-      throw new ProdutoNotFoundException("Não foi encontrado nenhum produto com o id " + id);
-    }
-    return produto;
+  public Produto create(Produto produto) {
+    validationProduct.validate(produto);
+    return outputPort.create(produto)
+        .orElseThrow(() -> new ProductCreationException(ProdutoConstant.ERROR_CREATING_PRODUCT));
   }
 }

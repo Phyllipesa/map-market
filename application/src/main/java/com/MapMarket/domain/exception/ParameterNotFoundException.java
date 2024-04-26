be264e7cbfa701de404a9bbe3c961f94ae952055
Phyllipe <phyllipe.sjc@hotmail.com>
2024-04-25 21:13:15 -0300

    Alteração de Exception para RuntimeException em ParameterNotFoundException

--- application/src/main/java/com/MapMarket/domain/exception/ParameterNotFoundException.java
+++ application/src/main/java/com/MapMarket/domain/exception/ParameterNotFoundException.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import java.io.Serial;
 
 @ResponseStatus(HttpStatus.BAD_REQUEST)
-public class ParameterNotFoundException extends Exception {
+public class ParameterNotFoundException extends RuntimeException {
   @Serial
   private static final long serialVersionUID = 1L;
 

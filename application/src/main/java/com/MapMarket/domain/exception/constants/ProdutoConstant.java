c35e168893004c1f5ba14b29b6cf39a1b0f92e81
Phyllipe <phyllipe.sjc@hotmail.com>
2024-04-25 21:19:35 -0300

    Adicionada constantes para ERROR_CREATING_PRODUCT, NULL_NOT_ALLOWED e NEGATIVE_NOT_ALLOWED

--- application/src/main/java/com/MapMarket/domain/exception/constants/ProdutoConstant.java
+++ application/src/main/java/com/MapMarket/domain/exception/constants/ProdutoConstant.java
@@ -1,11 +1,14 @@
 package com.MapMarket.domain.exception.constants;
 
 public class ProdutoConstant {
-  public static final String PRODUCT_NOT_FOUND = "Product not found :: ";
+  public static final String PRODUCT_NOT_FOUND = "Product not found with id ";
+  public static final String ERROR_CREATING_PRODUCT = "Error occurred while creating the product";
   public static final String REQUIRED_PARAMETER = "Required parameter '";
   public static final String IS_NULL_OR_BLANK = "' is null or blank!";
+  public static final String NULL_NOT_ALLOWED = "It is not allowed to persist a null object!";
   public static final double D_2 = 0.0;
   public static final int INT = 0;
+  public static final String NEGATIVE_NOT_ALLOWED = "It is not allowed negative numbers!";
   public static final String THIS_PRODUCT_IS_ALREADY_REGISTERED = "This Product is already registered ";
   public static final String PRODUCTS_NOT_FOUND = "Products not found";
 

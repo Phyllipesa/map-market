b430c7e7971e0dfa0d5ac4ccecec343200b99b0a
Phyllipe <phyllipe.sjc@hotmail.com>
2024-04-29 14:36:39 -0300

    Adicionado metodo "delete" em UseCase

--- application/src/main/java/com/MapMarket/domain/ports/input/UseCase.java
+++ application/src/main/java/com/MapMarket/domain/ports/input/UseCase.java
@@ -1,12 +1,10 @@
 package com.MapMarket.domain.ports.input;
 
-import com.MapMarket.domain.exception.ParameterNotFoundException;
-import com.MapMarket.domain.exception.ResourceNotFoundException;
-
 public interface UseCase<T> {
 
 //  List<T> findAll(Pageable pageable) throws ResourceNotFoundException;
   T findById(Long id);
   T create(T t);
   T update(Long id, T t);
+   void delete(Long id);
 }

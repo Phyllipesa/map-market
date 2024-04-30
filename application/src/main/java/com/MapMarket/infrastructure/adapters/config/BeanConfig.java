18244124861b79680ff68f8291471164feeeac32
Phyllipe <phyllipe.sjc@hotmail.com>
2024-04-30 19:22:08 -0300

    Refatoração da ModelMapper e ProdutoService em BeanConfig

--- application/src/main/java/com/MapMarket/infrastructure/adapters/config/BeanConfig.java
+++ application/src/main/java/com/MapMarket/infrastructure/adapters/config/BeanConfig.java
@@ -1,20 +1,34 @@
 package com.MapMarket.infrastructure.adapters.config;
 
+import com.MapMarket.application.rest.responseDto.ProdutoResponseDto;
 import com.MapMarket.domain.logic.ValidationProduct;
+import com.MapMarket.domain.models.Produto;
+import com.MapMarket.domain.ports.output.FindAllOutput;
 import com.MapMarket.domain.service.ProdutoService;
 import com.MapMarket.infrastructure.adapters.output.persistence.ProdutoPersistenceAdapter;
+import com.MapMarket.infrastructure.adapters.output.persistence.entities.ProdutoEntity;
 import com.MapMarket.infrastructure.adapters.output.persistence.mapper.EntityMapper;
 import com.MapMarket.infrastructure.adapters.output.persistence.repositories.ProdutoRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.web.PagedResourcesAssembler;
 
 @Configuration
 public class BeanConfig {
 
   @Bean
   public ModelMapper modelMapper() {
-    return new ModelMapper();
+    ModelMapper modelMapper = new ModelMapper();
+    modelMapper.typeMap(ProdutoEntity.class, Produto.class)
+        .addMappings(mapper -> {
+          mapper.map(ProdutoEntity::getId, Produto::setKey);
+        });
+    modelMapper.typeMap(Produto.class, ProdutoResponseDto.class)
+        .addMappings(mapper -> {
+          mapper.map(Produto::getKey, ProdutoResponseDto::setId);
+        });
+    return modelMapper;
   }
 
   @Bean
@@ -33,7 +47,13 @@ public class BeanConfig {
   }
 
   @Bean
-  public ProdutoService produtoService(ProdutoPersistenceAdapter produtoPersistenceAdapter, ValidationProduct validationProduct) {
-    return new ProdutoService(produtoPersistenceAdapter, validationProduct);
+  public ProdutoService produtoService(
+      ProdutoPersistenceAdapter produtoPersistenceAdapter,
+      ValidationProduct validationProduct,
+      PagedResourcesAssembler<ProdutoResponseDto> pagedResourcesAssembler,
+      EntityMapper entityMapper,
+      FindAllOutput<Produto> findAllOutput
+  ) {
+    return new ProdutoService(produtoPersistenceAdapter, findAllOutput, validationProduct, pagedResourcesAssembler, entityMapper);
   }
 }

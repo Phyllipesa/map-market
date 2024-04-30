b9cedc9f49d4780abc631515d8eb8b7f3bc36050
Phyllipe <phyllipe.sjc@hotmail.com>
2024-04-30 19:19:10 -0300

    Remoção do metodo findAll da interface OutputPort e refatoração da mesma para uma interface do tipo generica.

--- application/src/main/java/com/MapMarket/domain/ports/output/OutputPort.java
+++ application/src/main/java/com/MapMarket/domain/ports/output/OutputPort.java
@@ -1,15 +1,11 @@
 package com.MapMarket.domain.ports.output;
 
-import com.MapMarket.domain.models.Produto;
-
 import java.util.Optional;
 
 public interface OutputPort<T> {
 
-//  Page<Produto> findAll(P p);
-
-  Optional<Produto> findById(Long id);
-  Optional<Produto> create(T t);
-  Optional<Produto> update(Long id, T t);
+  Optional<T> findById(Long id);
+  Optional<T> create(T t);
+  Optional<T> update(Long id, T t);
   void delete(Long id);
 }

a22da4ece8030077bc6969ba34b3512994f8d84b
Phyllipe <phyllipe.sjc@hotmail.com>
2024-04-30 19:26:05 -0300

    Criação do metodo findAll via FindAllUseCase e Refatoração de todos os metodos

--- application/src/main/java/com/MapMarket/application/rest/ProdutoRestAdapter.java
+++ application/src/main/java/com/MapMarket/application/rest/ProdutoRestAdapter.java
@@ -2,10 +2,13 @@ package com.MapMarket.application.rest;
 
 import com.MapMarket.application.rest.requestDto.ProdutoRequestDto;
 import com.MapMarket.application.rest.responseDto.ProdutoResponseDto;
-import com.MapMarket.domain.exception.ParameterNotFoundException;
-import com.MapMarket.domain.models.Produto;
+import com.MapMarket.domain.ports.input.FindAllUseCase;
 import com.MapMarket.domain.ports.input.UseCase;
-import com.MapMarket.infrastructure.adapters.output.persistence.mapper.EntityMapper;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.PagedModel;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,43 +16,37 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("api/v1/produto")
 public class ProdutoRestAdapter {
 
-  private final UseCase<Produto> useCase;
-  private final EntityMapper entityMapper;
+  private final UseCase<ProdutoRequestDto, ProdutoResponseDto> useCase;
+  private final FindAllUseCase<ProdutoResponseDto> findAllUseCase;
 
-  public ProdutoRestAdapter(UseCase<Produto> useCase, EntityMapper entityMapper) {
+  public ProdutoRestAdapter(UseCase<ProdutoRequestDto, ProdutoResponseDto> useCase, FindAllUseCase<ProdutoResponseDto> findAllUseCase) {
+    this.findAllUseCase = findAllUseCase;
     this.useCase = useCase;
-    this.entityMapper = entityMapper;
   }
 
-//  @GetMapping
-//  public ResponseEntity<PagedModel<EntityModel<ProdutoResponseDto>>> findAll(
-//      @RequestParam(value = "page", defaultValue = "0") Integer page,
-//      @RequestParam(value = "size", defaultValue = "10") Integer size
-//  ) {
-//
-//    Pageable pageable = PageRequest.of(page, size, Sort.by(Sort.Direction.ASC, "nome"));
-//    List<Produto> produtoList = useCase.findAll(pageable);
-//    return ResponseEntity.ok(entityMapper.parseListObject(produtoList, ProdutoResponseDto.class));
-//  }
+  @GetMapping
+  public ResponseEntity<PagedModel<EntityModel<ProdutoResponseDto>>> findAll(
+      @RequestParam(value = "page", defaultValue = "0") Integer page,
+      @RequestParam(value = "size", defaultValue = "10") Integer size
+  ) {
+
+    Pageable pageable = PageRequest.of(page, size, Sort.by(Sort.Direction.ASC, "nome"));
+    return ResponseEntity.ok(findAllUseCase.findAll(pageable));
+  }
 
   @GetMapping("/{id}")
   public ResponseEntity<ProdutoResponseDto> findById(@PathVariable(value = "id")Long id) {
-    Produto produto = useCase.findById(id);
-    return ResponseEntity.ok(entityMapper.parseObject(produto, ProdutoResponseDto.class));
+    return ResponseEntity.ok(useCase.findById(id));
   }
 
   @PostMapping
-  public ResponseEntity<ProdutoResponseDto> create(@RequestBody ProdutoRequestDto produtoDto) throws ParameterNotFoundException {
-    Produto novoProduto = entityMapper.parseObject(produtoDto, Produto.class);
-    novoProduto = useCase.create(novoProduto);
-    return ResponseEntity.status(201).body(entityMapper.parseObject(novoProduto, ProdutoResponseDto.class));
+  public ResponseEntity<ProdutoResponseDto> create(@RequestBody ProdutoRequestDto produtoDto) {
+    return ResponseEntity.status(201).body(useCase.create(produtoDto));
   }
 
   @PutMapping("/{id}")
   public ResponseEntity<ProdutoResponseDto> update(@PathVariable(value = "id")Long id, @RequestBody ProdutoRequestDto produtoDto) {
-    Produto produto = entityMapper.parseObject(produtoDto, Produto.class);
-    produto = useCase.update(id, produto);
-    return ResponseEntity.ok(entityMapper.parseObject(produto, ProdutoResponseDto.class));
+    return ResponseEntity.ok(useCase.update(id, produtoDto));
   }
 
   @DeleteMapping("/{id}")

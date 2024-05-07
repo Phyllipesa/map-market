30e6a2a7fca2fb327973cd37cb7f59208eb8a036
Phyllipe <phyllipe.sjc@hotmail.com>
2024-05-07 17:52:31 -0300

    Refatoração no metodo update da interface OutputPort, remoção do Optional

--- application/src/main/java/com/MapMarket/domain/ports/output/OutputPort.java
+++ application/src/main/java/com/MapMarket/domain/ports/output/OutputPort.java
@@ -6,6 +6,6 @@ public interface OutputPort<T> {
 
   Optional<T> findById(Long id);
   Optional<T> create(T t);
-  Optional<T> update(Long id, T t);
+  T update(Long id, T t);
   void delete(Long id);
 }

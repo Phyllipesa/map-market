d60e9fbc788e2624b1f489e06dab11db085da693
Phyllipe <phyllipe.sjc@hotmail.com>
2024-05-16 19:06:44 -0300

    Criada CredentialsValidator

---
+++ application/src/main/java/com/MapMarket/domain/logic/CredentialsValidator.java
@@ -0,0 +1,14 @@
+package com.MapMarket.domain.logic;
+
+import com.MapMarket.application.rest.requestDto.AccountCredentialsVO;
+import com.MapMarket.domain.exception.ParameterNotFoundException;
+import com.MapMarket.domain.exception.constants.Constant;
+
+public class CredentialsValidator {
+
+  public void checkIfParamsIsNotNull(AccountCredentialsVO data) {
+    if (data == null || data.getUsername() == null || data.getUsername().isBlank()
+        || data.getPassword() == null || data.getPassword().isBlank())
+      throw new ParameterNotFoundException(Constant.REQUIRED_PARAMETER + "username/password" + Constant.IS_NULL_OR_BLANK );
+  }
+}

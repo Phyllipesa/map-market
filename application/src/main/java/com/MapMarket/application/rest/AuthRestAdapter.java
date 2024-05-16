7a1abf566542d0b448ecfa358f8eabb963b1001b
Phyllipe <phyllipe.sjc@hotmail.com>
2024-05-16 20:26:16 -0300

    Criado metodo refreshToken em AuthRestAdapter

--- application/src/main/java/com/MapMarket/application/rest/AuthRestAdapter.java
+++ application/src/main/java/com/MapMarket/application/rest/AuthRestAdapter.java
@@ -3,12 +3,10 @@ package com.MapMarket.application.rest;
 import com.MapMarket.application.rest.requestDto.AccountCredentialsVO;
 import com.MapMarket.application.rest.responseDto.TokenVO;
 import com.MapMarket.domain.logic.CredentialsValidator;
+import com.MapMarket.domain.logic.RefreshCredentialsValidator;
 import com.MapMarket.domain.ports.input.AuthUseCase;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/auth")
@@ -16,16 +14,25 @@ public class AuthRestAdapter {
 
   private final AuthUseCase authUseCase;
   private final CredentialsValidator credentialsValidator;
+  private final RefreshCredentialsValidator refreshCredentialsValidator;
 
-  public AuthRestAdapter(AuthUseCase authUseCase, CredentialsValidator credentialsValidator) {
+  public AuthRestAdapter(AuthUseCase authUseCase, CredentialsValidator credentialsValidator, RefreshCredentialsValidator refreshCredentialsValidator) {
     this.authUseCase = authUseCase;
     this.credentialsValidator = credentialsValidator;
+    this.refreshCredentialsValidator = refreshCredentialsValidator;
   }
 
   @PostMapping("/signin")
   public ResponseEntity<TokenVO> signin(@RequestBody AccountCredentialsVO data) {
     credentialsValidator.checkIfParamsIsNotNull(data);
     TokenVO token = authUseCase.signIn(data);
+    return ResponseEntity.ok(token);
+  }
+
+  @PutMapping("/refresh/{username}")
+  public ResponseEntity<TokenVO> refreshToken(@PathVariable("username") String username, @RequestHeader("Authorization") String refreshToken) {
+    refreshCredentialsValidator.checkIfParamsIsNotNull(username, refreshToken);
+    TokenVO token = authUseCase.refreshToken(username, refreshToken);
     return ResponseEntity.ok(token);
   }
 }

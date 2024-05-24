dadf94b22ecb9d20241950e5f8bd6da9cf358a6d
Phyllipe <phyllipe.sjc@hotmail.com>
2024-05-24 12:25:44 -0300

    Adicionadas as classes de request e response para auxilio no teste de integracao

---
+++ application/src/test/java/com/MapMarket/application/integrationTests/vo/ProdutoRequestDto.java
@@ -0,0 +1,36 @@
+package com.MapMarket.application.integrationTests.vo;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+public class ProdutoRequestDto implements Serializable {
+
+  @Serial
+  private static final long serialVersionUID = 1L;
+  private String nome;
+  private Double preco;
+
+  public ProdutoRequestDto() {
+  }
+
+  public ProdutoRequestDto(String nome, Double preco) {
+    this.nome = nome;
+    this.preco = preco;
+  }
+
+  public String getNome() {
+    return nome;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  public Double getPreco() {
+    return preco;
+  }
+
+  public void setPreco(Double preco) {
+    this.preco = preco;
+  }
+}

375d3ae0575da53689689ac952b0cdd66c3afbe4
Phyllipe <phyllipe.sjc@hotmail.com>
2024-05-24 10:17:31 -0300

    Adição do Common User na migration V18__Insert_Data_In_Users.sql

--- application/src/main/resources/db/migration/V18__Insert_Data_In_Users.sql
+++ application/src/main/resources/db/migration/V18__Insert_Data_In_Users.sql
@@ -1,3 +1,4 @@
 INSERT INTO `users` (`user_name`, `full_name`, `password`, `account_non_expired`, `account_non_locked`, `credentials_non_expired`, `enable`) VALUES
-    ('phyllipe', 'Phyllipe Soares', '6222cba3df71cdd84bca1b90af9297acc246505f13c4c2f9af193ea86cb70d4b7198b05b1d211762', b'1', b'1', b'1', b'1'),
-    ('guma', 'Gumercindo Vieira', '11055f55596a69a5bd8475a8eef0a777a6bf757048b3a3bc6d2055a177379cf6441f9bcc054a24a4', b'1', b'1', b'1', b'1');
+    ('phyllipe', 'Phyllipe Soares', 'c46cc8e47a0068834c41ec7fa621f945d86b411fa612e20f45d91f0ab9a9818d43c3f47de844f3cc', b'1', b'1', b'1', b'1'),
+    ('guma', 'Gumercindo Vieira', 'be897b9fb41cc44e320ac750d26460e23de3c86da9745fb5154344cf6cd2ca2f8e980027261a739e', b'1', b'1', b'1', b'1'),
+    ('cmUSER', 'test common user', '2385ed7f778fa4d510dad69485aa461554b5a7ecaacd7d734d14f9bacc3ff1ba66813ddbd4654983', b'1', b'1', b'1', b'1');

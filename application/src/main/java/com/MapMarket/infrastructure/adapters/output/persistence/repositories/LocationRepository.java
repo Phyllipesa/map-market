c0348f1d8d10dd8a0d614d7a40ba566ea6ad873c
Phyllipe <phyllipe.sjc@hotmail.com>
2024-06-10 15:58:36 -0300

    Criado metodo findAllLocationsWithProducts para retornar o produto em si e não apenas o id

--- application/src/main/java/com/MapMarket/infrastructure/adapters/output/persistence/repositories/LocationRepository.java
+++ application/src/main/java/com/MapMarket/infrastructure/adapters/output/persistence/repositories/LocationRepository.java
@@ -2,6 +2,11 @@ package com.MapMarket.infrastructure.adapters.output.persistence.repositories;
 
 import com.MapMarket.infrastructure.adapters.output.persistence.entities.LocationEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface LocationRepository extends JpaRepository<LocationEntity, Long> {
+  @Query("SELECT l FROM LocationEntity l JOIN FETCH l.product")
+  List<LocationEntity> findAllLocationsWithProducts();
 }

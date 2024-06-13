300b3aeb5a3978aacf27b6daaa3de0085d11dcf5
Phyllipe <phyllipe.sjc@hotmail.com>
2024-06-13 15:48:30 -0300

    Refatoração do script, adicioanndo novos atributos.

--- application/src/main/resources/db/migration/V3__Create_Table_ShelvingUnits.sql
+++ application/src/main/resources/db/migration/V3__Create_Table_ShelvingUnits.sql
@@ -1,4 +1,6 @@
 CREATE TABLE IF NOT EXISTS `shelving_units` (
     `id` INT AUTO_INCREMENT PRIMARY KEY,
-    `name` VARCHAR(10) NOT NULL
+    `unit` INT(20) NOT NULL,
+    `side_A` VARCHAR(180) NOT NULL,
+    `side_B` VARCHAR(180) NOT NULL
 );

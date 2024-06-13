7e06420c226da76d745ad994e256f5ffbb7c9eb7
Phyllipe <phyllipe.sjc@hotmail.com>
2024-06-13 15:31:31 -0300

    Adicionadas configuração das beans referente a Location.

--- application/src/main/java/com/MapMarket/infrastructure/adapters/config/BeanConfig.java
+++ application/src/main/java/com/MapMarket/infrastructure/adapters/config/BeanConfig.java
@@ -1,24 +1,30 @@
 package com.MapMarket.infrastructure.adapters.config;
 
 import com.MapMarket.application.rest.AuthRestAdapter;
+import com.MapMarket.application.rest.responseDto.LocationResponseDto;
 import com.MapMarket.application.rest.responseDto.ProductResponseDto;
 import com.MapMarket.domain.logic.CredentialsValidator;
 import com.MapMarket.domain.logic.ProductValidator;
 import com.MapMarket.domain.logic.RefreshCredentialsValidator;
+import com.MapMarket.domain.models.Location;
 import com.MapMarket.domain.models.Product;
 import com.MapMarket.domain.models.User;
 import com.MapMarket.domain.ports.output.FindAllOutput;
 import com.MapMarket.domain.ports.output.FindByUserNameOutputPort;
 import com.MapMarket.domain.ports.output.OutputPort;
 import com.MapMarket.domain.service.AuthService;
+import com.MapMarket.domain.service.LocationService;
 import com.MapMarket.domain.service.ProductService;
 import com.MapMarket.domain.service.UserService;
 import com.MapMarket.domain.service.security.jwt.JwtTokenFilter;
 import com.MapMarket.domain.service.security.jwt.JwtTokenProvider;
+import com.MapMarket.infrastructure.adapters.output.persistence.LocationPersistenceAdapter;
 import com.MapMarket.infrastructure.adapters.output.persistence.ProductPersistenceAdapter;
 import com.MapMarket.infrastructure.adapters.output.persistence.UserPersistenceAdapter;
+import com.MapMarket.infrastructure.adapters.output.persistence.entities.LocationEntity;
 import com.MapMarket.infrastructure.adapters.output.persistence.entities.ProductEntity;
 import com.MapMarket.infrastructure.adapters.output.persistence.mapper.EntityMapper;
+import com.MapMarket.infrastructure.adapters.output.persistence.repositories.LocationRepository;
 import com.MapMarket.infrastructure.adapters.output.persistence.repositories.ProductRepository;
 import com.MapMarket.infrastructure.adapters.output.persistence.repositories.UserRepository;
 import org.modelmapper.ModelMapper;
@@ -41,6 +47,15 @@ public class BeanConfig {
     modelMapper.typeMap(Product.class, ProductResponseDto.class)
         .addMappings(mapper -> {
           mapper.map(Product::getId, ProductResponseDto::setKey);
+        });
+
+    modelMapper.typeMap(LocationEntity.class, Location.class)
+        .addMappings(mapper -> {
+          mapper.map(LocationEntity::getId, Location::setId);
+        });
+    modelMapper.typeMap(Location.class, LocationResponseDto.class)
+        .addMappings(mapper -> {
+          mapper.map(Location::getId, LocationResponseDto::setKey);
         });
     return modelMapper;
   }
@@ -66,18 +81,12 @@ public class BeanConfig {
   }
 
   @Bean
-  public ProductPersistenceAdapter produtoPersistenceAdapter(ProductRepository productRepository, EntityMapper entityMapper) {
+  public ProductPersistenceAdapter productPersistenceAdapter(ProductRepository productRepository, EntityMapper entityMapper) {
     return new ProductPersistenceAdapter(productRepository, entityMapper);
   }
 
   @Bean
-  public ProductService produtoService(
-      OutputPort<Product> outputPort,
-      FindAllOutput<Product> findAllOutput,
-      ProductValidator productValidator,
-      PagedResourcesAssembler<ProductResponseDto> pagedResourcesAssembler,
-      EntityMapper entityMapper
-  ) {
+  public ProductService productService(OutputPort<Product> outputPort, FindAllOutput<Product> findAllOutput, ProductValidator productValidator, PagedResourcesAssembler<ProductResponseDto> pagedResourcesAssembler, EntityMapper entityMapper) {
     return new ProductService(outputPort, findAllOutput, productValidator, pagedResourcesAssembler, entityMapper);
   }
 
@@ -114,5 +123,15 @@ public class BeanConfig {
   @Bean
   public AuthRestAdapter authRestAdapter(AuthService authService, CredentialsValidator credentialsValidator, RefreshCredentialsValidator  refreshCredentialsValidator) {
     return new AuthRestAdapter(authService, credentialsValidator, refreshCredentialsValidator);
+  }
+
+  @Bean
+  public LocationPersistenceAdapter locationPersistenceAdapter(LocationRepository locationRepository, EntityMapper entityMapper) {
+    return new LocationPersistenceAdapter(locationRepository, entityMapper);
+  }
+
+  @Bean
+  public LocationService locationService(OutputPort<Location> outputPort, FindAllOutput<Location> findAllOutput, PagedResourcesAssembler<LocationResponseDto> pagedResourcesAssembler, EntityMapper entityMapper) {
+    return new LocationService(outputPort, findAllOutput, pagedResourcesAssembler, entityMapper);
   }
 }

b181d0ad6be5e5ef56083b9b1b647ba9a98fc861
Phyllipe <phyllipe.sjc@hotmail.com>
2024-06-13 15:24:55 -0300

    Adicionando pageable em findAllLocationsWithProducts

--- application/src/main/java/com/MapMarket/infrastructure/adapters/output/persistence/repositories/LocationRepository.java
+++ application/src/main/java/com/MapMarket/infrastructure/adapters/output/persistence/repositories/LocationRepository.java
@@ -1,12 +1,12 @@
 package com.MapMarket.infrastructure.adapters.output.persistence.repositories;
 
 import com.MapMarket.infrastructure.adapters.output.persistence.entities.LocationEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-import java.util.List;
-
 public interface LocationRepository extends JpaRepository<LocationEntity, Long> {
   @Query("SELECT l FROM LocationEntity l JOIN FETCH l.product")
-  List<LocationEntity> findAllLocationsWithProducts();
+  Page<LocationEntity> findAllLocationsWithProducts(Pageable pageable);
 }

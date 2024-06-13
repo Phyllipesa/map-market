5f703e202da9b81b9a79f76bbcc9d02b564b3ed9
Phyllipe <phyllipe.sjc@hotmail.com>
2024-06-13 15:48:59 -0300

    Adicionando novo padrão de insert

--- application/src/main/resources/db/migration/V4__Populate_Table_ShelvingUnits.sql
+++ application/src/main/resources/db/migration/V4__Populate_Table_ShelvingUnits.sql
@@ -1,4 +1,5 @@
-INSERT INTO `shelving_units` (`name`) VALUES
-('shelving A'),
-('shelving B'),
-('shelving C');
+INSERT INTO `shelving_units` (`unit`, `side_A`, `side_B`) VALUES
+(1, 'Grãos', 'Farinhas'),
+(2, 'Bolachas e Biscoitos', 'Sobremesas e Doces'),
+(3, 'Cereais', 'Laticínios'),
+(4, 'Higiene Pessoal/Perfumaria','Produtos de Limpeza');

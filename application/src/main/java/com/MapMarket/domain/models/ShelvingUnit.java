9ad34d378936b5e1873110f109d28525dda6036d
Phyllipe <phyllipe.sjc@hotmail.com>
2024-06-13 15:50:52 -0300

    Refatoração dos atributos da model ShelvingUnit.

--- application/src/main/java/com/MapMarket/domain/models/ShelvingUnit.java
+++ application/src/main/java/com/MapMarket/domain/models/ShelvingUnit.java
@@ -9,14 +9,18 @@ public class ShelvingUnit implements Serializable {
   @Serial
   private static final long serialVersionUID = 1L;
   private Long id;
-  private String name;
+  private Long unit;
+  private String sideA;
+  private String sideB;
 
   public ShelvingUnit() {
   }
 
-  public ShelvingUnit(Long id, String name) {
+  public ShelvingUnit(Long id, Long unit, String sideA, String sideB) {
     this.id = id;
-    this.name = name;
+    this.unit = unit;
+    this.sideA = sideA;
+    this.sideB = sideB;
   }
 
   public Long getId() {
@@ -27,23 +31,39 @@ public class ShelvingUnit implements Serializable {
     this.id = id;
   }
 
-  public String getName() {
-    return name;
+  public Long getUnit() {
+    return unit;
   }
 
-  public void setName(String name) {
-    this.name = name;
+  public void setUnit(Long unit) {
+    this.unit = unit;
+  }
+
+  public String getSideA() {
+    return sideA;
+  }
+
+  public void setSideA(String sideA) {
+    this.sideA = sideA;
+  }
+
+  public String getSideB() {
+    return sideB;
+  }
+
+  public void setSideB(String sideB) {
+    this.sideB = sideB;
   }
 
   @Override
   public boolean equals(Object o) {
     if (this == o) return true;
     if (!(o instanceof ShelvingUnit that)) return false;
-    return Objects.equals(id, that.id) && Objects.equals(name, that.name);
+    return Objects.equals(id, that.id) && Objects.equals(unit, that.unit) && Objects.equals(sideA, that.sideA) && Objects.equals(sideB, that.sideB);
   }
 
   @Override
   public int hashCode() {
-    return Objects.hash(id, name);
+    return Objects.hash(id, unit, sideA, sideB);
   }
 }

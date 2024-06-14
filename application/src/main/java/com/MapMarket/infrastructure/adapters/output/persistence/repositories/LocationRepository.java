e07fdbdb2a6ad57eac5ee30bad65ea5be5bd23af
Phyllipe <phyllipe.sjc@hotmail.com>
2024-06-14 15:11:51 -0300

    Modificando o metodo findLocationByProductId para retornar um Optional.

--- application/src/main/java/com/MapMarket/infrastructure/adapters/output/persistence/repositories/LocationRepository.java
+++ application/src/main/java/com/MapMarket/infrastructure/adapters/output/persistence/repositories/LocationRepository.java
@@ -5,11 +5,14 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface LocationRepository extends JpaRepository<LocationEntity, Long> {
   @Query("SELECT l FROM LocationEntity l JOIN FETCH l.product")
   Page<LocationEntity> findAllLocationsWithProducts(Pageable pageable);
 
-  @Query("SELECT l FROM LocationEntity l WHERE l.product_id =:id")
-  LocationEntity findLocationByProductId(Long id);
+  @Query("SELECT l FROM LocationEntity l WHERE l.product.id =:id")
+  Optional<LocationEntity> findLocationByProductId(@Param("id") Long id);
 }

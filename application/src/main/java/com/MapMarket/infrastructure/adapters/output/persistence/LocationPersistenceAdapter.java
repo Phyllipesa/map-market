f0a0c4fcdcb59d92728072df2d66a8fa7a00f040
Phyllipe <phyllipe.sjc@hotmail.com>
2024-06-14 15:19:17 -0300

    Adicionando o metodo FindLocationByProductIdOutputPort na LocationPersistenceAdapter

--- application/src/main/java/com/MapMarket/infrastructure/adapters/output/persistence/LocationPersistenceAdapter.java
+++ application/src/main/java/com/MapMarket/infrastructure/adapters/output/persistence/LocationPersistenceAdapter.java
@@ -2,6 +2,7 @@ package com.MapMarket.infrastructure.adapters.output.persistence;
 
 import com.MapMarket.domain.models.Location;
 import com.MapMarket.domain.ports.output.FindAllOutput;
+import com.MapMarket.domain.ports.output.FindLocationByProductIdOutputPort;
 import com.MapMarket.domain.ports.output.OutputPort;
 import com.MapMarket.infrastructure.adapters.output.persistence.entities.LocationEntity;
 import com.MapMarket.infrastructure.adapters.output.persistence.mapper.EntityMapper;
@@ -12,16 +13,16 @@ import org.springframework.data.domain.Pageable;
 import java.util.Optional;
 import java.util.logging.Logger;
 
-public class LocationPersistenceAdapter implements OutputPort<Location>, FindAllOutput<Location> {
+public class LocationPersistenceAdapter implements OutputPort<Location>, FindAllOutput<Location>, FindLocationByProductIdOutputPort<Location> {
 
     private final Logger logger = Logger.getLogger(LocationPersistenceAdapter.class.getName());
     private final LocationRepository locationRepository;
     private final EntityMapper entityMapper;
 
   public LocationPersistenceAdapter(LocationRepository locationRepository, EntityMapper entityMapper) {
-        this.locationRepository = locationRepository;
-        this.entityMapper = entityMapper;
-    }
+    this.locationRepository = locationRepository;
+    this.entityMapper = entityMapper;
+  }
 
   @Override
   public Page<Location> findAll(Pageable pageable) {
@@ -33,12 +34,20 @@ public class LocationPersistenceAdapter implements OutputPort<Location>, FindAll
 
   @Override
   public Optional<Location> findById(Long id) {
-    logger.info("locating a product by Id!");
+    logger.info("Finding a register by Id!");
     return Optional.ofNullable(entityMapper.parseObject(locationRepository.findById(id), Location.class));
+  }
+
+  public Optional<Location> findLocationByProductId(Long id) {
+    logger.info("Finding a register by product id!");
+    return Optional.ofNullable(entityMapper.parseObject(locationRepository.findLocationByProductId(id), Location.class));
   }
 
   @Override
   public Optional<Location> create(Location location) {
+    // Verificar se a ShelvingUnit existe.
+    // Verificar se o SIDE B existe.
+    // Verificar se o SIDE B existe.
     return Optional.empty();
   }
 

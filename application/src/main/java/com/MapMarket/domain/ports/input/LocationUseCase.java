236a96c5a451f6bf1201d14414a06eab606d7882
Phyllipe <phyllipe.sjc@hotmail.com>
2024-06-26 16:07:57 -0300

    Refatoração no LocationUseCase

--- application/src/main/java/com/MapMarket/domain/ports/input/LocationUseCase.java
+++ application/src/main/java/com/MapMarket/domain/ports/input/LocationUseCase.java
@@ -1,10 +1,8 @@
 package com.MapMarket.domain.ports.input;
 
-public interface LocationUseCase<Input, OutPut> {
+public interface LocationUseCase<OutPut> {
   OutPut findById(Long id);
   OutPut findLocationByProductId(Long id);
-  OutPut create(Input input);
-  OutPut update(Long locationId, Long productId);
+  OutPut subscribingProduct(Long locationId, Long productId);
   OutPut unsubscribingProduct(Long locationId);
-  void delete(Long id);
 }

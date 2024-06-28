88ee78e2c9641825f87fd9c068fa9e2509b0387f
Phyllipe <phyllipe.sjc@hotmail.com>
2024-06-28 18:11:44 -0300

    Criada FakeShelvingOutputPort

---
+++ application/src/test/java/com/MapMarket/application/unitTests/fakeClasses/FakeShelvingOutputPort.java
@@ -0,0 +1,51 @@
+package com.MapMarket.application.unitTests.fakeClasses;
+
+import com.MapMarket.application.unitTests.mocks.MockShelving;
+import com.MapMarket.domain.models.ShelvingUnit;
+import com.MapMarket.domain.ports.output.OutputPort;
+
+import java.util.Objects;
+import java.util.Optional;
+
+public class FakeShelvingOutputPort implements OutputPort<ShelvingUnit> {
+
+  MockShelving mockShelving = new MockShelving();
+
+  @Override
+  public Optional<ShelvingUnit> findById(Long id) {
+    if (id == 1) {
+      ShelvingUnit shelvingUnit = mockShelving.mockModel();
+      shelvingUnit.setId(1L);
+      return Optional.of(shelvingUnit);
+    }
+    return Optional.empty();
+  }
+
+  @Override
+  public Optional<ShelvingUnit> create(ShelvingUnit shelvingUnit) {
+    if (shelvingUnit == null || Objects.equals(shelvingUnit.getSideA(), "Exception")) {
+      return Optional.empty();
+    }
+    shelvingUnit.setId(2L);
+    return Optional.of(shelvingUnit);
+  }
+
+  @Override
+  public ShelvingUnit update(Long id, ShelvingUnit shelvingUnit) {
+    existResource(id);
+    shelvingUnit.setId(id);
+    shelvingUnit.setUnit(id);
+    shelvingUnit.setSideA("Updated");
+    shelvingUnit.setSideB("Updated");
+    return shelvingUnit;
+  }
+
+  @Override
+  public void delete(Long id) {
+  }
+
+  @Override
+  public boolean existResource(Long id) {
+    return id == 1;
+  }
+}

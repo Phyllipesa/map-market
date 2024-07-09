162a3379df7d7ec17d0930e3fa2b31f5a65fb8bf
Phyllipe <phyllipe.sjc@hotmail.com>
2024-07-09 20:09:56 -0300

    LocationResponseDto para os testes

---
+++ application/src/test/java/com/MapMarket/application/integrationTests/dto/LocationResponseDto.java
@@ -0,0 +1,85 @@
+package com.MapMarket.application.integrationTests.dto;
+
+import com.MapMarket.application.rest.responseDto.ProductResponseDto;
+import com.MapMarket.infrastructure.adapters.output.persistence.entities.ShelvingUnitEntity;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import org.springframework.hateoas.RepresentationModel;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+@JsonPropertyOrder({ "id", "shelvingUnit", "side", "part", "shelf", "product"})
+public class LocationResponseDto extends RepresentationModel<LocationResponseDto> implements Serializable {
+
+  @Serial
+  private static final long serialVersionUID = 1L;
+
+  @JsonProperty("id")
+  private Long key;
+  private Long shelvingUnit;
+  private String side;
+  private Long part;
+  private Long shelf;
+  private com.MapMarket.application.rest.responseDto.ProductResponseDto productResponseDto;
+
+  public LocationResponseDto() {
+  }
+
+  public LocationResponseDto(Long id, ShelvingUnitEntity shelvingUnit, String side, Long part, Long shelf, com.MapMarket.application.rest.responseDto.ProductResponseDto productResponseDto) {
+    this.key = id;
+    this.shelvingUnit = shelvingUnit.getId();
+    this.side = side;
+    this.part = part;
+    this.shelf = shelf;
+    this.productResponseDto = productResponseDto;
+  }
+
+  public Long getKey() {
+    return key;
+  }
+
+  public void setKey(Long key) {
+    this.key = key;
+  }
+
+  public Long getShelvingUnit() {
+    return shelvingUnit;
+  }
+
+  public void setShelvingUnit(Long shelvingUnit) {
+    this.shelvingUnit = shelvingUnit;
+  }
+
+  public String getSide() {
+    return side;
+  }
+
+  public void setSide(String side) {
+    this.side = side;
+  }
+
+  public Long getPart() {
+    return part;
+  }
+
+  public void setPart(Long part) {
+    this.part = part;
+  }
+
+  public Long getShelf() {
+    return shelf;
+  }
+
+  public void setShelf(Long shelf) {
+    this.shelf = shelf;
+  }
+
+  public com.MapMarket.application.rest.responseDto.ProductResponseDto getProduct() {
+    return productResponseDto;
+  }
+
+  public void setProduct(ProductResponseDto productResponseDto) {
+    this.productResponseDto = productResponseDto;
+  }
+}
